d835b968286641d0ce5416e76d393b69bf03d373
Alston Cobb <atc5nc@virginia.edu>
2021-03-08 08:38:38 -0300

    Created capibility for user to add images to a flat

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -45,7 +45,7 @@ class FlatsController < ApplicationController
     private
   
     def flat_params
-      params.require(:flat).permit(:price, :description, :adress, :availability_start_date, :availability_end_date, :title)
+      params.require(:flat).permit(:price, :description, :adress, :availability_start_date, :availability_end_date, :title, photos: [])
     end
 end
 
