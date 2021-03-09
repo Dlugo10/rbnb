14dd7872c86003577f3b86f6baf4cad2cef63fbf
David <71224300+Dlugo10@users.noreply.github.com>
2021-03-09 14:40:38 -0500

    Revert "Reservations checkout"

--- db/migrate/20210309145554_change_the_days_type.rb
+++
@@ -1,6 +0,0 @@
-class ChangeTheDaysType < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :reservations, :start_date
-    remove_column :reservations, :end_date
-  end
-end
--- db/migrate/20210309172655_add_start_date_to_reservation.rb
+++
@@ -1,6 +0,0 @@
-class AddStartDateToReservation < ActiveRecord::Migration[6.0]
-  def change
-    add_column :reservations, :start_date, :date
-    add_column :reservations, :end_date, :date
-  end
-end
