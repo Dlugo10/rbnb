1ecffaf28f3fd705f9daf64301ee699b3fef1430
Matheus Pereira Lopes <52245810+matlopes-lab@users.noreply.github.com>
2021-03-09 11:19:45 -0300

    New branch

--- db/schema.rb
+++ db/schema.rb
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 2021_03_08_112736) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.bigint "phone_number"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
